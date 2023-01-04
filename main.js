000b988a023a2d411b7d68553c1ead3c86972fb5
Helin <helin@bitbucket.org>
2023-01-04 16:13:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(el){
+    document.querySelector('.starbucks').src = el;  
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
